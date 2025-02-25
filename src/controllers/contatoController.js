675ae81e6c4f49044247b634af4f0448adcf7b4e
VictorS <victorsantos12dima@gmail.com>
2025-02-24 21:50:56 -0300

    feat: add route to delete contact

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -10,26 +10,26 @@ exports.register = async (req, res) => {
   try {
     const contato = new Contato(req.body);
     await contato.register();
-  
+
     if (contato.errors.length > 0) {
       req.flash('errors', contato.errors);
       req.session.save(() => res.redirect('back'));
       return
     };
-  
+
     req.flash('success', 'Contato registrado com sucesso.');
     req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
     return
   } catch (error) {
-      return res.render('404');
+    return res.render('404');
   }
 }
 
 exports.editIndex = async (req, res) => {
-  if(!req.params.id) return res.render('404');
+  if (!req.params.id) return res.render('404');
 
   const contato = await Contato.buscaPorId(req.params.id);
-  if(!contato) return res.render('404');
+  if (!contato) return res.render('404');
 
   res.render('contato', { contato })
 
@@ -37,23 +37,34 @@ exports.editIndex = async (req, res) => {
 
 exports.edit = async (req, res) => {
   try {
-    if(!req.params.id) return res.render('404');
+    if (!req.params.id) return res.render('404');
     const contato = new Contato(req.body);
-  
+
     await contato.edit(req.params.id);
-    
+
     if (contato.errors.length > 0) {
       req.flash('errors', contato.errors);
       req.session.save(() => res.redirect('back'));
       return
     };
-  
+
     req.flash('success', 'Contato editado com sucesso.');
     req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
     return
   } catch (error) {
     console.log(error);
-    
+
     res.render('404');
   }
-}
+};
+
+exports.delete = async (req, res) => {
+  if (!req.params.id) return res.render('404');
+
+  const contato = await Contato.delete(req.params.id);
+  if (!contato) return res.render('404');
+
+  req.flash('success', 'Contato apagado com sucesso.');
+  req.session.save(() => res.redirect(`back`));
+  return
+};
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -17,12 +17,7 @@ function Contato(body) {
   this.contato = null;
 };
 
-Contato.buscaPorId = async (id) => {
-  if(typeof id !== "string") return;
-  const user = await ContatoModel.findById(id);
 
-  return user
-}
 
 Contato.prototype.register = async function () {
   this.valida();
@@ -63,6 +58,30 @@ Contato.prototype.edit = async function (id) {
   this.valida();
   if(this.errors.length > 0) return;
   this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+}
+
+// Métodos estáticos
+
+Contato.buscaPorId = async (id) => {
+  if(typeof id !== "string") return;
+  const contato = await ContatoModel.findById(id);
+
+  return contato
+};
+
+Contato.buscaContatos = async () => {
+  const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 });
+
+  return contatos
+};
+
+Contato.delete = async (id) => {
+  if(typeof id !== "string") return;
+
+  const contato = await ContatoModel.findByIdAndDelete({_id: id})
+
+  return contato;
 }
 
 module.exports = Contato;
